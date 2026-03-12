b82d108dd5f2d1d24a17cfd112ae1dc9e4b4cc31
Ryusen9 <asratul02@gmail.com>
2026-03-12 23:58:48 +0600

    feat:initial project setup

---
+++ apps/api/src/main.ts
@@ -0,0 +1,10 @@
+import { NestFactory } from '@nestjs/core';
+import { AppModule } from './app.module';
+
+async function bootstrap() {
+  const app = await NestFactory.create(AppModule);
+  const port = Number(process.env.PORT);
+  await app.listen(port);
+  console.log(`Server is running on: http://localhost:${port}`);
+}
+bootstrap();
